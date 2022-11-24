cbd8043f42ab049fb7966d22496459e8e9a64b11
Alissa Cohan <alissacohan18@gmail.com>
2022-11-24 09:45:01 -0500

    refactor loops and add am/pm timeblocks

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,42 @@
+//Display current day
 var now = moment().format("dddd, MMM Do YYYY");
 $(currentDay).text(now);
 
 var tasks = {};
 
+//create elements for hour blocks, text area, and task save buttons
+var startTime = moment().startOf('day').add(8, 'hours');
 var i = 0;
+var hr
 while (i < 10) {
-  var timeContainer = document.createElement("section"); 
-  timeContainer.setAttribute('class',"time-block")
-  timeContainer.setAttribute('id',"section" + i)
-  document.querySelector(".container").appendChild(timeContainer); 
-  i++;
+  var timeContainer = document.createElement("section");
+  timeContainer.setAttribute("class", "row");
+  timeContainer.setAttribute("id", "section" + i);
+  document.querySelector(".container").appendChild(timeContainer);
 
-}
-
-var hr = 9;
-var j = 0;
-while (j < 10) {
+  //set time for each block
+  hr = moment(startTime)
+  .add(i, 'hours')
+  .format('hh:mm A'); 
+    
   var hrBlock = document.createElement("p");
-  hrBlock.innerText = hr;  
-  hrBlock.setAttribute('class',"hour")
-  hrBlock.setAttribute('id',"hr"+j)
-  document.getElementById("section"+j).appendChild(hrBlock);
-    hr++;
-    j++;
+  hrBlock.innerText = hr;
+  hrBlock.setAttribute("class", "hour col-1");
+  hrBlock.setAttribute("id", "hr" + i);
+  document.getElementById("section" + i).appendChild(hrBlock);
+
+  var textEl = document.createElement("textarea");
+  textEl.innerText = "tasks placeholder";
+  textEl.setAttribute("class", "description col-10");
+  textEl.setAttribute("id", "text" + i);
+  document.getElementById("section" + i).appendChild(textEl);
+
+  var saveEl = document.createElement("button");
+  saveEl.innerText = "save";
+  saveEl.setAttribute("class", "saveBtn col-1");
+  saveEl.setAttribute("id", "save" + i);
+   document.getElementById("section" + i).appendChild(saveEl);
+  i++;
 }
 
-var k = 0;
-while (k < 10) {
-  var textEl = document.createElement("textarea");
-  textEl.innerText = "tasks placeholder";  
-  textEl.setAttribute('class',"row")
-  textEl.setAttribute('id',"text"+j)
-  document.getElementById("section"+k).appendChild(textEl);
-    k++;
-}
+//event listeners for adding tasks
