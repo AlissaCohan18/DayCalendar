64f4c1ac7bfbff3f0f0be10a87dca038775520d7
Alissa Cohan <alissacohan18@gmail.com>
2022-11-25 15:02:49 -0500

    refactored

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,11 @@
-//Display current day
+//Display current day at top of page
 var now = moment().format("dddd, MMM Do YYYY");
 $(currentDay).text(now);
 //set variables
 var allTasks = [];
-var tasks = {};
 var timeStamp;
-var taskDescription;
-var timeKeeper;
-var taskId 
-var saveId
+var taskId;
+var saveId;
 
 //create elements for hour blocks
 var startTime = moment().startOf("day").add(8, "hours");
@@ -19,81 +16,63 @@ while (i < 10) {
   timeContainer.setAttribute("class", "time-block row");
   timeContainer.setAttribute("id", "section" + i);
   document.querySelector(".container").appendChild(timeContainer);
-  //set time for each block
+  //set a.m. & p.m. for time blocks and military time for id comparisons
   hr = moment(startTime).add(i, "hours").format("hh:mm A");
   hr2 = moment(startTime).add(i, "hours").format("HH:mm");
-
   var hrBlock = document.createElement("p");
   hrBlock.innerText = hr;
   hrBlock.setAttribute("class", "hour col-1");
   hrBlock.setAttribute("id", "hr" + i);
   document.getElementById("section" + i).appendChild(hrBlock);
-  //text fields for each block
+  //create text fields for each block
   var textEl = document.createElement("textarea");
   textEl.setAttribute("id", hr2);
-  textEl.setAttribute("id", "cd"+i);
+  textEl.setAttribute("id", "cd" + i);
   document.getElementById("section" + i).appendChild(textEl);
   //set class for past/present/future
-  var now = moment().format("HH");
+  now = moment().format("HH");
   var nowObjEl = moment(now, "HH");
   var timeStamp = moment(hr2, "HH");
   if (nowObjEl.isAfter(timeStamp) === true) {
-    textEl.setAttribute("class", "description col-10 tOD past");
+    textEl.setAttribute("class", "description col-10 past");
   } else if (nowObjEl.isBefore(timeStamp) === true) {
-    textEl.setAttribute("class", "description col-10 tOD future");
+    textEl.setAttribute("class", "description col-10 future");
   } else {
-    textEl.setAttribute("class", "description col-10 tOD present");
+    textEl.setAttribute("class", "description col-10 present");
   }
   //save buttons
   var saveEl = document.createElement("button");
   saveEl.innerText = "save";
   saveEl.setAttribute("class", "saveBtn col-1");
-  saveEl.setAttribute("type", "submit");
   saveEl.setAttribute("id", i);
   document.getElementById("section" + i).appendChild(saveEl);
   i++;
 }
 
-//recall tasks from local storage
-//var loadTasks = function () {
+//recalls and loads from local storage, or creates empty object if nothing stored
 allTasks = JSON.parse(localStorage.getItem("tasks"));
-console.log(allTasks);
-//if nothing in localStorage, create a new object to track all task status arrays
 if (!allTasks) {
-  console.log("no tasks");
-  allTasks =[]
+  allTasks = [];
 } else {
   j = 0;
   while (j < allTasks.length) {
-    console.log(allTasks[j].timeStamp);
-    //document.getElementById((allTasks[j].timeStamp))
     textEl = document.getElementById(allTasks[j].timeStamp);
     textEl.innerText = allTasks[j].taskDescription;
-    console.log(allTasks[j].taskDescription);
     j++;
   }
 }
 
-//update task description
+//update task description when saved
 $(".description").change(function () {
   newTasks = {
- timeStamp: $(this).attr("id"),
- taskDescription: $(this).val(),
-};
-taskId = $(this).attr("id")
- 
-
-//event listener for save/submit
-$(".saveBtn").click(function () {
-  saveId = $(this).attr("id")
-var newId = "cd"+saveId
-if (newId === taskId) {
-  allTasks.push(newTasks);
-  localStorage.setItem("tasks", JSON.stringify(allTasks));  
-} else {
-  console.log("not a match")
-
-}
-
-});
+    timeStamp: $(this).attr("id"),
+    taskDescription: $(this).val(),
+  };
+  //event listener for saving
+  $(".saveBtn").click(function () {
+    if ("cd" + $(this).attr("id") === newTasks.timeStamp) {
+      allTasks.push(newTasks);
+      localStorage.setItem("tasks", JSON.stringify(allTasks));
+    }
+  });
 });
