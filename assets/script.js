72a13736037f6b5570fd97562ebba50d207b0775
Alissa Cohan <alissacohan18@gmail.com>
2022-11-25 09:54:10 -0500

    color coded timeblocks by past/present/future; initial local storage code added

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,14 @@ var now = moment().format("dddd, MMM Do YYYY");
 $(currentDay).text(now);
 
 var tasks = {};
+var timeStamp;
+var taskDescription;
+var timeKeeper;
 
 //create elements for hour blocks, text area, and task save buttons
-var startTime = moment().startOf('day').add(8, 'hours');
+var startTime = moment().startOf("day").add(8, "hours");
 var i = 0;
-var hr
+var hr;
 while (i < 10) {
   var timeContainer = document.createElement("section");
   timeContainer.setAttribute("class", "row");
@@ -15,10 +18,9 @@ while (i < 10) {
   document.querySelector(".container").appendChild(timeContainer);
 
   //set time for each block
-  hr = moment(startTime)
-  .add(i, 'hours')
-  .format('hh:mm A'); 
-    
+  hr = moment(startTime).add(i, "hours").format("hh:mm A");
+  hr2 = moment(startTime).add(i, "hours").format("HH:mm");
+
   var hrBlock = document.createElement("p");
   hrBlock.innerText = hr;
   hrBlock.setAttribute("class", "hour col-1");
@@ -27,16 +29,49 @@ while (i < 10) {
 
   var textEl = document.createElement("textarea");
   textEl.innerText = "tasks placeholder";
-  textEl.setAttribute("class", "description col-10");
-  textEl.setAttribute("id", "text" + i);
+  textEl.setAttribute("id", hr2);
   document.getElementById("section" + i).appendChild(textEl);
+  //set class past/present/future
+  var now = moment().format("HH");
+  var nowObjEl = moment(now, "HH");
+  var timeStamp = moment(hr2, "HH");
+  if (nowObjEl.isAfter(timeStamp) === true) {
+    textEl.setAttribute("class", "description col-10 tOD past");
+  } else if (nowObjEl.isBefore(timeStamp) === true) {
+    textEl.setAttribute("class", "description col-10 tOD future");
+  } else {
+    textEl.setAttribute("class", "description col-10 tOD present");
+  }
 
   var saveEl = document.createElement("button");
   saveEl.innerText = "save";
   saveEl.setAttribute("class", "saveBtn col-1");
   saveEl.setAttribute("id", "save" + i);
-   document.getElementById("section" + i).appendChild(saveEl);
+  document.getElementById("section" + i).appendChild(saveEl);
   i++;
 }
+
+//recall tasks from local storage
+var loadTasks = function () {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+  console.log(tasks);
+  // if nothing in localStorage, create a new object to track all task status arrays
+  if (!tasks) {
+    console.log("no tasks");
+    tasks = {
+      timeStamp: [],
+      taskDescription: [],
+    };
+  }
+  // loop over object properties
+  // $.each(tasks, function (list, arr) {
+  // then loop over sub-array
+  //  arr.forEach(function (task) {
+  //    createTask(task.text, task.date, list);
+  //  });
+  // });
+};
+
+loadTasks();
 
 //event listeners for adding tasks
