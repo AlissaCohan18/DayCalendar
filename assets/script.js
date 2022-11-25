6ec4f474bde5c6e1e61def298e7718572b965979
Alissa Cohan <alissacohan18@gmail.com>
2022-11-25 12:21:31 -0500

    local storage saves and re-loads tasks)

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
 //Display current day
 var now = moment().format("dddd, MMM Do YYYY");
 $(currentDay).text(now);
-
+//set variables
+var allTasks = [];
 var tasks = {};
 var timeStamp;
 var taskDescription;
 var timeKeeper;
 
-//create elements for hour blocks, text area, and task save buttons
+//create elements for hour blocks
 var startTime = moment().startOf("day").add(8, "hours");
 var i = 0;
 var hr;
@@ -16,7 +17,6 @@ while (i < 10) {
   timeContainer.setAttribute("class", "row");
   timeContainer.setAttribute("id", "section" + i);
   document.querySelector(".container").appendChild(timeContainer);
-
   //set time for each block
   hr = moment(startTime).add(i, "hours").format("hh:mm A");
   hr2 = moment(startTime).add(i, "hours").format("HH:mm");
@@ -26,12 +26,11 @@ while (i < 10) {
   hrBlock.setAttribute("class", "hour col-1");
   hrBlock.setAttribute("id", "hr" + i);
   document.getElementById("section" + i).appendChild(hrBlock);
-
+  //text fields for each block
   var textEl = document.createElement("textarea");
-  textEl.innerText = "tasks placeholder";
   textEl.setAttribute("id", hr2);
   document.getElementById("section" + i).appendChild(textEl);
-  //set class past/present/future
+  //set class for past/present/future
   var now = moment().format("HH");
   var nowObjEl = moment(now, "HH");
   var timeStamp = moment(hr2, "HH");
@@ -42,36 +41,48 @@ while (i < 10) {
   } else {
     textEl.setAttribute("class", "description col-10 tOD present");
   }
-
+  //save buttons
   var saveEl = document.createElement("button");
   saveEl.innerText = "save";
   saveEl.setAttribute("class", "saveBtn col-1");
-  saveEl.setAttribute("id", "save" + i);
+  saveEl.setAttribute("type", "submit");
+  saveEl.setAttribute("id", i);
   document.getElementById("section" + i).appendChild(saveEl);
   i++;
 }
 
 //recall tasks from local storage
-var loadTasks = function () {
-  tasks = JSON.parse(localStorage.getItem("tasks"));
-  console.log(tasks);
-  // if nothing in localStorage, create a new object to track all task status arrays
-  if (!tasks) {
-    console.log("no tasks");
-    tasks = {
-      timeStamp: [],
-      taskDescription: [],
-    };
+//var loadTasks = function () {
+allTasks = JSON.parse(localStorage.getItem("tasks"));
+console.log(allTasks);
+//if nothing in localStorage, create a new object to track all task status arrays
+if (!allTasks) {
+  console.log("no tasks");
+  allTasks =[]
+} else {
+  j = 0;
+  while (j < allTasks.length) {
+    console.log(allTasks[j].timeStamp);
+    //document.getElementById((allTasks[j].timeStamp))
+    textEl = document.getElementById(allTasks[j].timeStamp);
+    textEl.innerText = allTasks[j].taskDescription;
+    console.log(allTasks[j].taskDescription);
+    j++;
   }
-  // loop over object properties
-  // $.each(tasks, function (list, arr) {
-  // then loop over sub-array
-  //  arr.forEach(function (task) {
-  //    createTask(task.text, task.date, list);
-  //  });
-  // });
-};
+}
 
-loadTasks();
+//event listeners; load tasks
+$(".description").change(function () {
+     newTasks = {
+    timeStamp: $(this).attr("id"),
+    taskDescription: $(this).val(),
+  };
+  allTasks.push(newTasks);
+  console.log(allTasks);
+  localStorage.setItem("tasks", JSON.stringify(allTasks));
+   });
 
-//event listeners for adding tasks
+$(".saveBtn").click(function () {
+    console.log($(this).attr("id"))
+    
+});
